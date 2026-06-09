a0da140f909322d5c3a3d74171d4a1a24b6cf564
V DINESH KARTHIK <123288599+vdineshkarthik@users.noreply.github.com>
2026-06-09 19:19:48 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -104,16 +104,7 @@ if (card) {
   // ------------------------------------------------------------------------------ //
   // Parallax (apply parallax effect to any element with a data-speed attribute) Start
   // ------------------------------------------------------------------------------ //
-  gsap.to("[data-speed]", {
-    y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window) ,
-    ease: "none",
-    scrollTrigger: {
-      start: 0,
-      end: "max",
-      invalidateOnRefresh: true,
-      scrub: 0
-    }
-  });
+
   // --------------------------------------------- //
   // Parallax End
   // --------------------------------------------- //
